945c33669f07207b61033bfc00c032a260d72810
Gabriela Leite <gabrielamiranda11.10@gmail.com>
2023-09-17 19:11:26 -0300

    feat: add prop

--- src/components/Divider/styles.ts
+++ src/components/Divider/styles.ts
@@ -1,8 +1,10 @@
 import styled from "styled-components";
 import theme from "../../styles/theme";
+import { DividerProps } from "./type";
 
-export const Divider = styled.div`
-  background-color: ${theme.colors.greyLight};
+export const Divider = styled.div<DividerProps>`
+  background-color: ${({ isDark }) =>
+    isDark ? theme.colors.black : theme.colors.greyLight};
   height: 1px;
   width: 100%;
 `;

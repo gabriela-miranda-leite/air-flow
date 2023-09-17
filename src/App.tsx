c78305d23d738732e3c0fb930c70732198d0f833
Gabriela Leite <gabrielamiranda11.10@gmail.com>
2023-09-17 19:53:45 -0300

    feat: create WelcomeAccount responsive

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,13 @@ import { ThemeProvider } from "styled-components";
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/globalStyles";
 
-import { SocialMediaButton } from "./components";
+import { WelcomeAccount } from "./pages";
 
 const App = () => {
   return (
     <ThemeProvider theme={{ theme }}>
       <GlobalStyle />
-      <SocialMediaButton action="SignIn" />
-      <SocialMediaButton action="Login" />
+      <WelcomeAccount />
     </ThemeProvider>
   );
 };

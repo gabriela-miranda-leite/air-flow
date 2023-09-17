3c0188e6ab68296dc9ed16ef09d0db8d8cd05c9e
Gabriela Leite <gabrielamiranda11.10@gmail.com>
2023-09-17 14:41:45 -0300

    feat: create Icons

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { ThemeProvider } from "styled-components";
 
 import theme from "./styles/theme";
 import GlobalStyle from "./styles/globalStyles";
+import image from "./assets/images/IconLogo.webp";
 
 const App = () => {
   return (
     <ThemeProvider theme={{ theme }}>
       <GlobalStyle />
-      <h1>oi</h1>
+      <h1>oiaaa</h1>
+      <img src={image} alt="aaa teste" />
     </ThemeProvider>
   );
 };

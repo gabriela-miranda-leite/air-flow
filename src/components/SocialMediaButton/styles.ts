ae14f88ea8ac45dbcd223347a8f25d393c7e467e
Gabriela Leite <gabrielamiranda11.10@gmail.com>
2023-09-17 19:12:23 -0300

    style: adjust color button social media button

--- src/components/SocialMediaButton/styles.ts
+++ src/components/SocialMediaButton/styles.ts
@@ -1,22 +1,34 @@
 import styled from "styled-components";
 import theme from "../../styles/theme";
 
-export const Button = styled.button`
+import { SocialMediaButtonProps } from "./types";
+
+export const Button = styled.button<SocialMediaButtonProps>`
   align-items: center;
-  background-color: ${theme.colors.white};
+  background-color: ${({ action }) =>
+    action === "Login" ? theme.colors.google : theme.colors.white};
   border: none;
   border-radius: ${theme.borderRadius.lg};
-  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.15);
-  column-gap: ${theme.spacing["2xl"]};
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+  color: ${({ action }) =>
+    action === "Login" ? theme.colors.white : theme.colors.black};
+  column-gap: ${theme.spacing.xl};
   display: flex;
   font-family: ${theme.fonts.primary};
   font-size: ${theme.fonts.sizes.lg};
   justify-content: center;
   padding: ${theme.spacing.sm} ${theme.spacing.lg};
-  width: 35.2rem;
+  width: 100%;
+
+  -moz-user-select: none;
+  -webkit-user-select: none;
+  -ms-user-select: none;
+  user-select: none;
 
   &:active {
-    box-shadow: 0px 4px 0px ${theme.colors.white};
+    box-shadow: 0px 4px 4px 0px
+      ${({ action }) =>
+        action === "Login" ? theme.colors.google : theme.colors.white};
     transform: translateY(1px);
   }
 

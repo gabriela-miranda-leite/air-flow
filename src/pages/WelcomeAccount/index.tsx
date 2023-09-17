8d4a30370b5fee07955dbc8ec6a7bbb1d9ecd3fe
Gabriela Leite <gabrielamiranda11.10@gmail.com>
2023-09-17 19:40:24 -0300

    feat: create WelcomeAccount in Desktop

--- src/pages/WelcomeAccount/index.tsx
+++ src/pages/WelcomeAccount/index.tsx
@@ -2,17 +2,35 @@ import { Logo, SocialMediaButton } from "../../components";
 import * as S from "./styles";
 
 export const WelcomeAccount = () => {
-  return (
-    <S.Container>
+  const Mobile = () => (
+    <S.ContainerMobile>
       <Logo size="large" />
 
       <S.ContainerAccount>
         <S.Label>Acesse o painel com sua da Google.</S.Label>
         <SocialMediaButton action="Login" />
 
-        <S.Label>Ou crie sua conta com a sua conta da Google.</S.Label>
+        <S.Label>Crie sua conta utilizando sua conta do Google.</S.Label>
         <SocialMediaButton action="SignIn" />
       </S.ContainerAccount>
-    </S.Container>
+    </S.ContainerMobile>
   );
+
+  const Desktop = () => (
+    <S.ContainerDesktop>
+      <S.ContainerInfo>
+        <Logo size="large" />
+        <S.ContainerAccount>
+          <S.Label>Acesse o painel com sua da Google.</S.Label>
+          <SocialMediaButton action="Login" />
+
+          <S.Label>Crie sua conta utilizando sua conta do Google.</S.Label>
+          <SocialMediaButton action="SignIn" />
+        </S.ContainerAccount>
+      </S.ContainerInfo>
+      <S.BackgroundImage />
+    </S.ContainerDesktop>
+  );
+
+  return Desktop();
 };

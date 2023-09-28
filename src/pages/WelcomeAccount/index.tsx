9d2d8a17299fcfc36143350c627af9cce04fcd06
Gabriela <gabrielamiranda11.10@gmail.com>
2023-09-28 08:17:52 -0300

    feat: add firebase

--- src/pages/WelcomeAccount/index.tsx
+++ src/pages/WelcomeAccount/index.tsx
@@ -1,17 +1,32 @@
+import React, { useState, useEffect } from "react";
+import { signInWithPopup } from "firebase/auth";
+import { auth, provider } from "../../auth/firebase";
+import { useNavigate } from "react-router-dom";
+
 import { Logo, SocialMediaButton } from "../../components";
 import * as S from "./styles";
 
 export const WelcomeAccount = () => {
+  const history = useNavigate();
+  const [authData, setAuthData] = useState();
+
+  const handleClick = () => {
+    signInWithPopup(auth, provider).then((data) => {
+      console.log(data);
+      history("/Dashboard");
+    });
+  };
+
   const Mobile = () => (
     <S.ContainerMobile>
       <Logo size="large" />
 
       <S.ContainerAccount>
         <S.Label>Acesse o painel com sua da Google.</S.Label>
-        <SocialMediaButton action="Login" />
+        <SocialMediaButton action="Login" onClick={handleClick} />
 
         <S.Label>Crie sua conta utilizando sua conta do Google.</S.Label>
-        <SocialMediaButton action="SignIn" />
+        <SocialMediaButton action="SignIn" onClick={handleClick} />
       </S.ContainerAccount>
     </S.ContainerMobile>
   );
@@ -22,10 +37,10 @@ export const WelcomeAccount = () => {
         <Logo size="large" />
         <S.ContainerAccount>
           <S.Label>Acesse o painel com sua da Google.</S.Label>
-          <SocialMediaButton action="Login" />
+          <SocialMediaButton action="Login" onClick={handleClick} />
 
           <S.Label>Crie sua conta utilizando sua conta do Google.</S.Label>
-          <SocialMediaButton action="SignIn" />
+          <SocialMediaButton action="SignIn" onClick={handleClick} />
         </S.ContainerAccount>
       </S.ContainerInfo>
       <S.BackgroundImage />

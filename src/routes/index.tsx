0341bcc66a687dd2196f698e5ad358eca2be4142
Gabriela <gabrielamiranda11.10@gmail.com>
2023-09-28 08:16:08 -0300

    feat: add route Dashboard

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,12 +4,13 @@ import {
   Routes as RoutesNavigation,
 } from "react-router-dom";
 
-import { WelcomeAccount } from "../pages";
+import { Dashboard, WelcomeAccount } from "../pages";
 
 export const Routes = () => (
   <BrowserRouter>
     <RoutesNavigation>
       <Route path="/" element={<WelcomeAccount />} />
+      <Route path="/Dashboard" element={<Dashboard />} />
     </RoutesNavigation>
   </BrowserRouter>
 );

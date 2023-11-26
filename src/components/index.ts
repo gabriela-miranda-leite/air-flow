26f61640cea0f4b6122408015c32aeafdc02fb77
Gabriela <gabrielamiranda11.10@gmail.com>
2023-11-25 23:55:03 -0300

    feat: add SideMenuIcon

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,7 +2,7 @@ import { Divider } from "./Divider/styles";
 import * as Icons from "./Icons";
 import { Logo } from "./Logo";
 import { MenuItemIcon } from "./MenuItemIcon";
-// import { SideMenuIcon } from "./SideMenuIcon";
+import { SideMenuIcon } from "./SideMenuIcon";
 import { SocialMediaButton } from "./SocialMediaButton";
 
-export { Divider, Icons, Logo, MenuItemIcon, SocialMediaButton };
+export { Divider, Icons, Logo, MenuItemIcon, SideMenuIcon, SocialMediaButton };
